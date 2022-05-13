a059bffd61ed8af279046f94b60c62f02ac39130
Skool Media <skoolmedia@Skools-MacBook-Pro.local>
2022-05-13 14:11:13 +0100

    Finished website design and cofigurations

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home/index.html', {})
+
+def about(request):
+    return render(request, 'home/about.html', {})
+    
+
+def solutions(request):
+    return render (request, 'home/solutions.html', {})
+
+def centers (request):
+    return render(request, 'home/centers.html', {})
+
+def contact(request):
+    return render(request, 'home/contact.html', {})
